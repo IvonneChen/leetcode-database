6dc0806305fa8079123b6accd51243d538738631
diaosj <groundhacker@gmail.com>
2017-09-21 19:23:18 +0800

    185,accepted

--- department_highest_salary.sql
+++ department_highest_salary.sql
@@ -1,4 +1,6 @@
-SELECT t3.Name AS Department, t1.Name AS Employee, t1.Salary AS Salary
-FROM Employee t1
-JOIN (SELECT DepartmentId, max(Salary) AS Salary FROM Employee GROUP BY DepartmentId) t2 ON t1.DepartmentId = t2.DepartmentId AND t1.Salary = t2.Salary
-JOIN Department t3 ON t1.DepartmentId = t3.Id
+SELECT D.Name AS Department, E.Name AS Employee, E.Salary AS Salary 
+FROM Employee E 
+INNER JOIN Department D ON E.DepartmentId = D.Id 
+WHERE (SELECT COUNT(DISTINCT Salary) FROM Employee 
+       WHERE DepartmentId = E.DepartmentId AND Salary > E.Salary) < 3
+ORDER by E.DepartmentId, E.Salary DESC;
